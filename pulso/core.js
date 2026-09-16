b54cb36f2a4bd8535c7a85c6400b5f86b06ba9b9
jssalinas127-cell <jssalinas127@gmail.com>
2026-09-15 19:12:11 -0500

    Pulso: core.js

---
+++ pulso/core.js
@@ -0,0 +1,6 @@
+/* =========================================================================
+   DATOS: preguntas (questions.js), motor (engine.js), capa interpretativa
+   (configurations.js) y reporte (report.js) inlineados aquí para que el
+   archivo sea una sola página autocontenida, distribuible como lead magnet
+   sin dependencias externas.
+   ========================================================================= */
